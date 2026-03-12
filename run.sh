2425539bc88fa63a01f1a0f7d761c1a1ebfc14bd
Finnegannnn <thomsen-f@hotmail.com>
2026-03-12 13:10:51 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,18 @@
 #Shell Script to run all the various language scripts in one
+echo "Beggining now...."
+
+#python
+echo "Python3: \n"
+python3 main.py
+
+#C++
+echo "C++: \n"
+
+#Go
+echo "Golang: \n"
+
+#Rust
+echo "Rust: \n"
+
+#Assembly 
+echo "Assembly: \n"
