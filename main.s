59dad7511d234c1f3a2d6770ec6e827f69b46e65
Finnegannnn <thomsen-f@hotmail.com>
2026-03-12 14:11:11 -0600

    Update and rename main.asm to main.s

---
+++ main.s
@@ -0,0 +1,18 @@
+.section  .data
+msg:
+    .asciz "Hello World!"   
+msg_len = . - msg             
+
+.section .text
+.globl _start              
+_start:
+
+    movl $4, %eax              
+    movl $1, %ebx            
+    movl $msg, %ecx            
+    movl $msg_len, %edx        
+    int $0x80                 
+
+    movl $1, %eax              
+    xorl %ebx, %ebx        
+    int $0x80                
