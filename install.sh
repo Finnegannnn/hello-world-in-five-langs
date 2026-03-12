e52ce4e733b97d3cdb97213cbb9f23733e1f9805
Finnegannnn <thomsen-f@hotmail.com>
2026-03-12 13:24:58 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #Instaltion script for all required depencencies for the project
+
+#python dependiencies
 sudo chmod a+x run.sh
 sudo apt-get install python3
+
+#c++ dependencies
 sudo apt update && sudo apt install build-essential
 gcc --version
+
+#Golang Depencencies
+sudo apt-get install wget
+wget https://go.dev/dl/go1.26.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.26.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
