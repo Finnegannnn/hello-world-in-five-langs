85f98db508281633dd75e0370201f7095095a643
Finnegannnn <thomsen-f@hotmail.com>
2026-03-12 13:26:23 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,6 @@ gcc --version
 #Golang Depencencies
 sudo apt-get install wget
 wget https://go.dev/dl/go1.26.1.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.26.1.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.26.1.linux-amd64.tar.gz
+sudo export PATH=$PATH:/usr/local/go/bin
 go version
