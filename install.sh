c17141b156b8bffef4b16239eab20ee9b18bdfae
Finnegannnn <thomsen-f@hotmail.com>
2026-03-12 13:16:45 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #Instaltion script for all required depencencies for the project
 sudo chmod a+x run.sh
 sudo apt-get install python3
+sudo apt update && sudo apt install build-essential
+gcc --version
